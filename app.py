9d7bd6021510088db7d9622642aa05eac2f74518
MattinglyJen <pulleyjrenee@gmail.com>
2021-04-07 14:18:32 -0400

    adding updates

---
+++ app.py
@@ -0,0 +1,79 @@
+
+
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+    )
+
+    """Query the dates and temperature observations for the most active station for the last year"""
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    return jsonify(prcp)
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(measurements.prcp).all()
+
+
+
+
+
+
+
+
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    date_prcp = []
+    for name, age, sex in results:
+        passenger_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
